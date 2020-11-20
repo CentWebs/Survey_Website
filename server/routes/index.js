896dd3eaed62107a70296858fe48dd4c20aa5841
Jasper <lolitsamejasper@gmail.com>
2020-11-20 16:05:02 -0500

    added login/register views and routing, need fixing on routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,27 @@ FileName : app.js
 var express = require('express');
 var router = express.Router();
 
+let indexController = require('../controllers/index')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Welcome survey' });
 });
+
+/* GET Route for displaying the LOGIN page*/
+router.get('/login ', indexController.displayLoginPage);
+
+/* POST Route for processing the REGISTER page*/
+router.post('/login', indexController.processLoginPage);
+
+/* GET Route for displaying the REGISTER page*/
+router.get('/register ', indexController.displayRegisterPage);
+
+/* POST Route for processing the REGISTER page*/
+router.post('/register', indexController.processRegisterPage);
+
+/* GET to perform USER LOGOUT*/
+router.get('/logout', indexController.performLogout);
 
 
 /* GET Surevey page. */
