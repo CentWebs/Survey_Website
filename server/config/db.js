0a15187f04ec532c147c281ec68b3316335d5fb5
Christine Kweri <ckweri@my.centennialcollege.ca>
2020-12-11 20:06:09 -0500

    I haved added additional comments

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+/*--
+Author: CentWebs
+Date: 11-11-2020
+FileName : db.js
+
+Where to connect from (local,hosted db)
+
+*/
+
+module.exports =
+{
+   //"URI": "mongodb://localhost/contact"
+   "URI":"mongodb+srv://botjasper:wUL0z1sWDV8ei4vF@mongodbserver.x2t68.azure.mongodb.net/express_portfolio?retryWrites=true&w=majority",
+   "Secret": 'SomeSecret'
+}
